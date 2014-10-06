6de9e0bc7bb0ebeba8151a4a46d64c278b8f582e
crimsonr-974 <info@crimsonr.com>
2014-10-06 22:27:21 +0200

    Add the list view

--- js/App.js
+++ js/App.js
@@ -9,6 +9,7 @@ var app = (function(){
 		init: function(){
 			this.content = $("#content");
 			this.todos = new api.collections.ToDos();
+			ViewsFactory.menu();
 			return this;
 		},
 		changeContent: function(el){
@@ -24,9 +25,37 @@ var app = (function(){
 				});
 			}
 			return this.menuView;
+		},
+		list: function(archive){
+			if(!this.listView){
+				this.listView = new api.views.list({
+					model: api.todos
+				})
+			}
 		}
 	};
-	var Router = Backbone.Router.extend({});
+	var Router = Backbone.Router.extend({
+		routes: {
+			"archive": "archive",
+			"new": "newToDo",
+			"edit/:index": "editToDo",
+			"delete/:index": "delteTodo",
+			"": "list"
+		},
+		list: function(archive){
+			var view = ViewsFactory.list();
+			api
+			.title(archive ? "Archive" : "Your ToDos:")
+			.changeContent(view.$el);
+			view.setMode(archive ? "archive" : null).render();
+		},
+		archive: function(){
+			this.list(true);
+		},
+		newToDo: function(){},
+		editToDo: function(index){},
+		delteTodo: function(index){}
+	});
 	api.router = new Router();
 
 	return api;
